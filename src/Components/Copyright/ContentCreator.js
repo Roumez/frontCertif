6cdae0cb1cdd2927b61edbe630c55c094cf2dfd4
Roumez <remi.roumezy@hotmail.fr>
2021-08-20 14:20:48 +0200

    Dashboard

--- src/Components/Copyright/ContentCreator.js
+++ src/Components/Copyright/ContentCreator.js
@@ -13,8 +13,8 @@ const ContentCreator = () => {
         <label htmlFor="image">Cover page</label>
         <input type="text" className="link" placeholder="https://www.google.com/cover-game.jpg" />
 
-         <label htmlFor="title">Key game</label>
-        <input type="text" id="title" placeholder="Key Game" />
+         <label htmlFor="title">Price game</label>
+        <input type="text" id="title" placeholder="Price Game" />
 
         <label htmlFor="description">Description Game</label>
         <textarea id="description game" placeholder="Description game"></textarea>
--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Dashboard.css'
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className="title">
+      <h1>My games</h1>
     </div>
   );
 };
