5a3df5ca92956b45e7d2e8a2808423cb3fbcdc25
Roumez <remi.roumezy@hotmail.fr>
2021-08-20 13:18:57 +0200

    key game

--- src/Components/Copyright/ContentCreator.js
+++ src/Components/Copyright/ContentCreator.js
@@ -4,19 +4,22 @@ import './ContentCreator.css'
 const ContentCreator = () => {
   return (
     <>
-       <h1 className="title-form">sell your video game</h1>
+       <h1 className="title-form">Create your video game</h1>
 
       <form className="container-form">
-        <label htmlFor="title">Titre</label>
+        <label htmlFor="title">Title</label>
         <input type="text" id="title" placeholder="Game name" />
 
         <label htmlFor="image">Cover page</label>
         <input type="text" className="link" placeholder="https://www.google.com/cover-game.jpg" />
 
+         <label htmlFor="title">Key game</label>
+        <input type="text" id="title" placeholder="Key Game" />
+
         <label htmlFor="description">Description Game</label>
         <textarea id="description game" placeholder="Description game"></textarea>
 
-        <button>Sell your game</button>
+        <button>Create your game</button>
       </form>
     </>
   );
